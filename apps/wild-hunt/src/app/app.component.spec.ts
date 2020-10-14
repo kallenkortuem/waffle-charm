fbbff90488dbe5d7ddab02b956b9d49bfd163dc0
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-10-14 00:44:54 -0500

    add react application

--- apps/wild-hunt/src/app/app.component.spec.ts
+++ apps/wild-hunt/src/app/app.component.spec.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [HttpClientModule],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);

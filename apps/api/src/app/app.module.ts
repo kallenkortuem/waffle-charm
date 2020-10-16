9e4b5d09d52377993ebd9e78724b03ad895f8ead
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-10-16 09:34:43 -0500

    add match nestjs module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SummonerModule } from './summoner/summoner.module';
+import { MatchModule } from './match/match.module';
 
 @Module({
   imports: [
     HttpModule,
     ConfigModule.forRoot({ isGlobal: true }),
     SummonerModule,
+    MatchModule,
   ],
   controllers: [AppController],
   providers: [AppService],

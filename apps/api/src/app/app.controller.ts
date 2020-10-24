0aaaf0de2c7f9de09769e103e5c186d1e3df473f
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-10-23 23:46:56 -0500

    remove message refeence

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get } from '@nestjs/common'
-import { Message } from '@waffle-charm/api-interfaces'
 import { AppService } from './app.service'
 
 @Controller()
@@ -7,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
-  getData(): Message {
+  getData(): any {
     return this.appService.getData()
   }
 }

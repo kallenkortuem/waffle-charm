3b7cfa2e818a595047824e9619075bc762963332
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-10-30 10:32:47 -0500

    add more material-ui

--- apps/crow-storm/src/app/app.tsx
+++ apps/crow-storm/src/app/app.tsx
@@ -1,3 +1,6 @@
+import Container from '@material-ui/core/Container'
+import Paper from '@material-ui/core/Paper'
+import Badge from '@material-ui/core/Badge'
 import {
   ChampionData,
   ChampionDataDragon,
@@ -6,9 +9,24 @@ import {
 } from '@waffle-charm/api-interfaces'
 import React, { useEffect, useState } from 'react'
 import './app.scss'
+import ImageAvatar from './image-avatar/image-avatar'
+import PrimarySearchAppBar from './primary-search-bar/primary-search-bar'
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
 
 const SUMMONER_NAME_KEY = 'summonerName'
 const initialSummonerName = sessionStorage.getItem(SUMMONER_NAME_KEY)
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+        padding: theme.spacing(2),
+      },
+    },
+  })
+)
 
 export const App = (): React.ReactElement => {
   const [summoner, setSummoner] = useState<SummonerDTO>()
@@ -16,13 +34,20 @@ export const App = (): React.ReactElement => {
   const [masteries, setMasteries] = useState<ChampionMasteryDTO[]>()
   const [championData, setChampionData] = useState<ChampionDataDragon>()
 
-  function getSummoner(e: React.FormEvent<HTMLFormElement>): void {
-    e.preventDefault()
-    fetch(`/api/summoner/${summonerName}`)
-      .then((_) => _.json())
-      .then((value) => {
-        setSummoner(value)
-      })
+  const classes = useStyles()
+
+  function getSummoner(event: React.FormEvent<HTMLFormElement>): void {
+    event.preventDefault()
+    if (summonerName) {
+      fetch(`/api/summoner/${summonerName}`)
+        .then((_) => _.json())
+        .then((value) => {
+          setSummoner(value)
+        })
+    } else {
+      setSummoner(undefined)
+      setMasteries([])
+    }
   }
 
   function handleSetSummonerName(event: React.ChangeEvent<HTMLInputElement>) {
@@ -36,6 +61,9 @@ export const App = (): React.ReactElement => {
       fetch(`/api/mastery/by-summoner/${summoner.id}`)
         .then((_) => _.json())
         .then((value) => {
+          if (value.statusCode) {
+            return
+          }
           setMasteries(value)
         })
     }
@@ -51,43 +79,71 @@ export const App = (): React.ReactElement => {
       })
   }, [])
 
-  const mappedData: { [key: number]: ChampionData } = Object.entries(
-    championData?.data || []
-  ).reduce((accumulated, [_, entry]) => {
-    accumulated[entry.key] = entry
-    return accumulated
-  }, {})
+  const mappedData: { [key: number]: ChampionData } =
+    Object.entries(championData?.data || []).reduce(
+      (accumulated, [_, entry]) => {
+        accumulated[entry.key] = entry
+        return accumulated
+      },
+      {}
+    ) || {}
+
+  const groupedMasteries: {
+    [key: number]: ChampionMasteryDTO[]
+  } =
+    masteries?.reduce((accum, current) => {
+      if (accum[current.championLevel]) {
+        accum[current.championLevel].push(current)
+      } else {
+        accum[current.championLevel] = [current]
+      }
+      return accum
+    }, {}) || {}
 
   return (
-    <div id="wrapper">
-      <div>
-        <h1>Welcome to crow-storm!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt=""
-        />
-      </div>
-      <form onSubmit={getSummoner}>
-        <input
-          type="text"
-          value={summonerName}
-          onChange={handleSetSummonerName}
-        />
-        <button type="submit">Search</button>
-      </form>
-      <pre>Summoner: {JSON.stringify(summoner, null, 4)}</pre>
-      <ul>
-        {masteries?.map((mastery) => (
-          <li key={mastery.championId}>
-            {mastery.championLevel} | {mappedData[mastery.championId]?.name} |{' '}
-            {mastery.tokensEarned ? mastery.tokensEarned : ''}
-          </li>
-        ))}
-      </ul>
-      <pre>Champion Masteries: {JSON.stringify(masteries?.[0], null, 4)}</pre>
-      <pre>Champion Data: {JSON.stringify(mappedData?.[9], null, 4)}</pre>
-    </div>
+    <>
+      <PrimarySearchAppBar
+        query={summonerName}
+        onQueryChange={handleSetSummonerName}
+        onSearch={getSummoner}
+      ></PrimarySearchAppBar>
+      <Container maxWidth="sm">
+        <div>
+          <h1>Champion Mastery</h1>
+        </div>
+
+        {Object.entries(groupedMasteries)
+          .sort(([a], [b]) => parseInt(b) - parseInt(a))
+          .map(([key, group]) => (
+            <div key={key}>
+              <h2>Mastery {key} </h2>
+              <p>Count {group?.length}</p>
+              <div className={classes.root}>
+                <Paper className={'mastery-group'}>
+                  {group?.map((mastery) => (
+                    <Badge
+                      key={mastery.championId}
+                      badgeContent={mastery.tokensEarned}
+                      color="secondary"
+                    >
+                      <ImageAvatar
+                        src={`http://ddragon.leagueoflegends.com/cdn/10.22.1/img/champion/${
+                          mappedData[mastery.championId]?.image?.full
+                        }`}
+                        alt={mappedData[mastery.championId]?.name}
+                      ></ImageAvatar>
+                    </Badge>
+                  ))}
+                </Paper>
+              </div>
+            </div>
+          ))}
+
+        <pre>Summoner: {JSON.stringify(summoner, null, 4)}</pre>
+        <pre>Champion Masteries: {JSON.stringify(masteries?.[0], null, 4)}</pre>
+        <pre>Champion Data: {JSON.stringify(mappedData?.[9], null, 4)}</pre>
+      </Container>
+    </>
   )
 }
 
--- apps/crow-storm/src/main.tsx
+++ apps/crow-storm/src/main.tsx
@@ -1,11 +1,14 @@
+import CssBaseline from '@material-ui/core/CssBaseline'
+import 'fontsource-roboto'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import App from './app/app'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </React.StrictMode>,
   document.getElementById('root')
 )

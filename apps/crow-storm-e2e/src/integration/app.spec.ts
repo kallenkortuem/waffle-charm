89ab732dd6e42364e93ee25b5db70ded666759a2
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-11-12 19:07:32 -0600

    and toggle back

--- apps/crow-storm-e2e/src/integration/app.spec.ts
+++ apps/crow-storm-e2e/src/integration/app.spec.ts
@@ -23,5 +23,12 @@ describe('crow-storm', () => {
       expect($body).to.have.css('background-color', 'rgb(250, 250, 250)')
       expect($body).to.have.css('color', 'rgba(0, 0, 0, 0.87)')
     })
+
+    getDarkModeToggle().click()
+
+    getBody().should(($body) => {
+      expect($body).to.have.css('background-color', 'rgb(48, 48, 48)')
+      expect($body).to.have.css('color', 'rgb(255, 255, 255)')
+    })
   })
 })

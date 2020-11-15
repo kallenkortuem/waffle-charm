75fa78f9187d8e4bb0b2da55530da72d436e72c4
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-11-15 03:11:05 -0600

    fix merge issue

--- apps/crow-storm-e2e/src/integration/mastery/mastery-grid-group.spec.ts
+++ apps/crow-storm-e2e/src/integration/mastery/mastery-grid-group.spec.ts
@@ -7,9 +7,10 @@ describe('mastery grid group', () => {
   beforeEach(() => cy.visit('/'))
 
   it('should respect the mastery filter', () => {
-    getMasteryGridGroup(7).should('exist')
-    getMasteryLevelFilter(7).click().blur()
-    getMasteryGridGroup(7).should('not.exist')
-    getMasteryLevelFilter(7).click().blur()
+    getMasteryGridGroup(2).should('not.exist')
+    getMasteryLevelFilter(2).click().blur()
+    getMasteryGridGroup(2).should('exist')
+    getMasteryLevelFilter(2).click().blur()
+    getMasteryGridGroup(2).should('not.exist')
   })
 })

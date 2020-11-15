00842c4f075c8859874567a473c1f38bb4b6c2d9
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-11-15 11:46:17 -0600

    format

--- libs/react/mastery/src/lib/testing/MasteryFilter.spec.tsx
+++ libs/react/mastery/src/lib/testing/MasteryFilter.spec.tsx
@@ -4,24 +4,32 @@ import { render } from '@testing-library/react'
 import MasteryFilter from '../MasteryFilter'
 
 describe('MasteryFilter', () => {
-  const allTags = ["Tank", "Fighter", "Support", "Assasin", "Mage", "Marksman"]
-  const tag = "Assasin"
-  const layout = "module"
-  const masteryLevels = ["1"]
-  const handleSetTag= () => { return null }
-  const handleLayoutChange= () => { return null }
-  const handleSetMasteryLevels = () => { return null }
+  const allTags = ['Tank', 'Fighter', 'Support', 'Assasin', 'Mage', 'Marksman']
+  const tag = 'Assasin'
+  const layout = 'module'
+  const masteryLevels = ['1']
+  const handleSetTag = () => {
+    return null
+  }
+  const handleLayoutChange = () => {
+    return null
+  }
+  const handleSetMasteryLevels = () => {
+    return null
+  }
 
   it('should render successfully', () => {
-    const { baseElement } = render(<MasteryFilter
-      allTags={allTags}
-      tag={tag}
-      layout={layout}
-      masteryLevels={masteryLevels}
-      onTagChange={handleSetTag}
-      onLayoutChange={handleLayoutChange}
-      onMasteryLevelsChange={handleSetMasteryLevels}
-      />)
+    const { baseElement } = render(
+      <MasteryFilter
+        allTags={allTags}
+        tag={tag}
+        layout={layout}
+        masteryLevels={masteryLevels}
+        onTagChange={handleSetTag}
+        onLayoutChange={handleLayoutChange}
+        onMasteryLevelsChange={handleSetMasteryLevels}
+      />
+    )
     expect(baseElement).toBeTruthy()
   })
 })

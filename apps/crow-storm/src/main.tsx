70cf2aac3f20103b292028ed99d75f224771657e
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-12-05 17:35:28 -0600

    feature(waffle-charm): add redux-persist

--- apps/crow-storm/src/main.tsx
+++ apps/crow-storm/src/main.tsx
@@ -1,19 +1,21 @@
 import 'fontsource-roboto'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistedStore, store } from './app/+store'
 import App from './app/app'
 import './i18n'
-
-import store from './app/+store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

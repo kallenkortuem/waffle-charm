12d2f5df31eb1580815b18909370b22d39bcd838
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-12-24 11:10:03 -0600

    refactor(store): move store index to common

--- apps/crow-storm/src/app/app.spec.tsx
+++ apps/crow-storm/src/app/app.spec.tsx
@@ -3,7 +3,7 @@ import { enableFetchMocks } from 'jest-fetch-mock'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { store } from './+store'
+import { store } from '@waffle-charm/store'
 import App from './app'
 import './i18n'
 

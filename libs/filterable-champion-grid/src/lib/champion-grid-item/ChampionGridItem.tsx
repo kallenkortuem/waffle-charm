e50d8799e5ff63d5293189c9a1a1e9fdd1f0cd96
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-12-24 20:40:11 -0600

    feat(search): add overscan to the grid

--- libs/filterable-champion-grid/src/lib/champion-grid-item/ChampionGridItem.tsx
+++ libs/filterable-champion-grid/src/lib/champion-grid-item/ChampionGridItem.tsx
@@ -1,4 +1,3 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core'
 import { MasteryCard } from '@waffle-charm/mastery'
 import {
   selectChampionEntities,
@@ -9,27 +8,11 @@ import {
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      padding: theme.spacing(2, 1),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      overflow: 'hidden',
-    },
-    avatar: {
-      margin: theme.spacing(0, 0, 1, 0),
-    },
-  })
-)
-
 /* eslint-disable-next-line */
 export interface ChampionGridItemProps {
   championId: string
 }
 export function ChampionGridItem(props: ChampionGridItemProps) {
-  const classes = useStyles()
   const { championId } = props
   const champion = useSelector(selectChampionEntities)[championId]
   const version = useSelector(selectLolVersion)

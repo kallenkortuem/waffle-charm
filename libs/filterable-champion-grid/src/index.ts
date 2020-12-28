b82d4f27a98e8fd38563bc469cb47e350a00c7eb
Kallen Kortuem <kallenjkortuem@gmail.com>
2020-12-27 19:10:25 -0600

    pr cleanup

--- libs/filterable-champion-grid/src/index.ts
+++ libs/filterable-champion-grid/src/index.ts
@@ -5,6 +5,5 @@ export * from './lib/champion-grid-filter/ChampionGridFilter'
 export * from './lib/champion-grid-item/ChampionGridItem'
 export * from './lib/champion-grid-search/ChampionGridSearch'
 export * from './lib/champion-grid-sort-select/ChampionGridSortSelect'
-export * from './lib/champion-list-container/ChampionListContainer'
 
 export default root.FilterableChampionGrid

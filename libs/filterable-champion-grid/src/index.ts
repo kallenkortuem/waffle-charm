8d43d80d99292c3b5c466444f519c56a4b20fc10
Kallen Kortuem <kallenjkortuem@gmail.com>
2021-02-08 20:31:46 -0600

    chore: remove unused code

--- libs/filterable-champion-grid/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as root from './lib/FilterableChampionGrid'
-
-export * from './lib/champion-grid-container/ChampionGridContainer'
-export * from './lib/champion-grid-filter/ChampionGridFilter'
-export * from './lib/champion-grid-item/ChampionGridItem'
-export * from './lib/champion-grid-sort-select/ChampionGridSortSelect'
-
-export default root.FilterableChampionGrid

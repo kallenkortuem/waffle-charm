db7c8bf4ce6bf8d68607dd3432ef493e0368856f
Kallen Kortuem <kallenjkortuem@gmail.com>
2021-02-15 10:39:08 -0600

    test(tag): fix autocomplete

--- apps/crow-storm/src/app/containers/tag-autocomplete/TagAutocomplete.spec.tsx
+++ apps/crow-storm/src/app/containers/tag-autocomplete/TagAutocomplete.spec.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
 import { render } from '@testing-library/react'
-
+import { store } from '@waffle-charm/store'
+import React from 'react'
+import { Provider } from 'react-redux'
+import '../../../i18n'
 import TagAutocomplete from './TagAutocomplete'
 
 describe('TagAutocomplete', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<TagAutocomplete />)
+    const { baseElement } = render(
+      <Provider store={store}>
+        <TagAutocomplete />
+      </Provider>
+    )
     expect(baseElement).toBeTruthy()
   })
 })

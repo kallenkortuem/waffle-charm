048be1a7098e39063274fafb1786d8cdd49ee73c
Kallen Kortuem <kallenjkortuem@gmail.com>
2021-04-08 12:09:55 -0500

    feat: add favorite and bans

--- apps/crow-storm/src/app/containers/mastery-list-view/MasteryListViewV2.tsx
+++ apps/crow-storm/src/app/containers/mastery-list-view/MasteryListViewV2.tsx
@@ -1,14 +1,20 @@
-import { Link, Paper } from '@material-ui/core'
+import { IconButton, Link, Paper } from '@material-ui/core'
 import {
   DataGrid,
   GridColDef,
   GridValueFormatterParams,
 } from '@material-ui/data-grid'
+import BanIcon from '@material-ui/icons/Block'
+import FavoriteIcon from '@material-ui/icons/FavoriteBorder'
 import { createSelector } from '@reduxjs/toolkit'
 import { getChampionInfoUrl } from '@waffle-charm/champions'
 import { MasteryProgress } from '@waffle-charm/mastery'
 import {
+  bansActions,
   ChampionEntity,
+  createSelectBansById,
+  createSelectFavoriteById,
+  favoriteActions,
   MasteryEntity,
   selectChampionEntities,
   selectChampionVendor,
@@ -18,10 +24,10 @@ import {
 import { formatDistanceToNowStrict } from 'date-fns'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 type NarrowedGridColDef = GridColDef & {
-  field: keyof ChampionEntity | keyof MasteryEntity
+  field: keyof ChampionEntity | keyof MasteryEntity | string
 }
 
 const selectRows = createSelector(
@@ -30,10 +36,54 @@ const selectRows = createSelector(
   selectMasteryEntities,
   (championIds, champions, masteries) => {
     return championIds.map((id) => {
-      return { id, ...champions[id], ...masteries[id] }
+      return { championId: id, ...champions[id], ...masteries[id] }
     })
   }
 )
+
+const PickBan = (props: { championId: string }): React.ReactElement => {
+  const { championId } = props
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+  const champion = useSelector(selectChampionEntities)[championId]
+  const selectBansById = createSelectBansById()
+  const isBaned = useSelector((state) => selectBansById(state, champion?.key))
+  const selectFavoriteById = createSelectFavoriteById()
+  const isFavorite = useSelector((state) =>
+    selectFavoriteById(state, champion?.key)
+  )
+
+  const handleBanClick = () => {
+    const a = isBaned
+      ? bansActions.remove(championId)
+      : bansActions.add({ id: championId })
+
+    dispatch(a)
+  }
+
+  const handleFavoriteClick = () => {
+    const a = isFavorite
+      ? favoriteActions.remove(championId)
+      : favoriteActions.add({ id: championId })
+
+    dispatch(a)
+  }
+
+  return (
+    <>
+      <IconButton
+        aria-label={t('championFavoriteCTA')}
+        onClick={handleFavoriteClick}
+      >
+        <FavoriteIcon color={isFavorite ? 'secondary' : 'disabled'} />
+      </IconButton>
+
+      <IconButton aria-label={t('championBanCTA')} onClick={handleBanClick}>
+        <BanIcon color={isBaned ? 'error' : 'disabled'} />
+      </IconButton>
+    </>
+  )
+}
 
 export default function MasteryListViewV2(): React.ReactElement {
   const { t } = useTranslation()
@@ -56,18 +106,29 @@ export default function MasteryListViewV2(): React.ReactElement {
         </Link>
       ),
     },
-    { field: 'championLevel', headerName: t('masteryLevel'), width: 158 },
+    {
+      field: ' ',
+      headerName: ' ',
+      disableColumnMenu: true,
+      hideSortIcons: true,
+      sortable: false,
+      width: 130,
+      renderCell: ({ row }) => {
+        return <PickBan championId={row?.championId || row?.id} />
+      },
+    },
+    { field: 'championLevel', headerName: t('masteryLevel'), width: 100 },
     {
       field: 'championPoints',
       headerName: t('totalPoints'),
-      width: 140,
+      width: 120,
       valueFormatter: ({ value }: GridValueFormatterParams) =>
         value?.toLocaleString(),
     },
     {
       field: 'lastPlayTime',
       headerName: t('lastPlayTime'),
-      width: 180,
+      width: 100,
       valueFormatter: ({ value }: GridValueFormatterParams) =>
         value ? formatDistanceToNowStrict((value as number) || 0) : '',
     },

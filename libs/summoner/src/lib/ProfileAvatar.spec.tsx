f6125368ed05a341334c6b5d59d006bdd0a47abd
Kallen Kortuem <kallenjkortuem@gmail.com>
2021-08-07 13:03:38 -0500

    fix unit test

--- libs/summoner/src/lib/ProfileAvatar.spec.tsx
+++ libs/summoner/src/lib/ProfileAvatar.spec.tsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react'
 import { enablei18nMocks } from '@waffle-charm/testing-utils'
+import * as store from '@waffle-charm/store'
 import React from 'react'
 import ProfileAvatar from './ProfileAvatar'
 
 enablei18nMocks()
+
+jest
+  .spyOn(store, 'useLolVersion')
+  .mockImplementation(() => ({ lolVersion: '10.3.1' }))
 
 describe('ProfileAvatar', () => {
   it('should render successfully', () => {
